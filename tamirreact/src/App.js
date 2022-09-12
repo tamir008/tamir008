fa5eaf6b05badba92fd87063090070f9c62effe1
tuka97 <76560459+tuka97@users.noreply.github.com>
2022-09-12 16:16:13 +0800

    Tamir 3

--- tamirreact/src/App.js
+++ tamirreact/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Person = (props) => {
+  return (
+    <>
+    <h1>Name: {props.name}</h1>
+    <h2>Last Name: {props.lastName}</h2>
+    <h2>Age: {props.age}</h2>
+    </>
+  )
+}
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Person
+        name='John' 
+        LastName='Doe'
+        age= {25} 
+      />
+  <Person name='Mary' LastName={'Doe'age=24} />
+  </div>
   );
 }
 
