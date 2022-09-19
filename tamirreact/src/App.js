f5988736cc27bcda23c4c8e4289c2ea523addc0f
tuka97 <76560459+tuka97@users.noreply.github.com>
2022-09-19 16:26:59 +0800

    2-r video ehluulev

--- tamirreact/src/App.js
+++ tamirreact/src/App.js
@@ -1,23 +1,14 @@
 import './App.css';
 
-const Person = (props) => {
-  return (
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Last Name: {props.lastName}</h2>
-    <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 const App = () => {
+const name = 'John';
+const isNameShowing = true;
+
   return (
     <div className="App">
-      <Person
-        name='John' 
-        LastName='Doe'
-        age= {25} 
-      />
-  <Person name='Mary' LastName={'Doe'age=24} />
+      <buttun on Click={() => setCounter}>-</buttun>
+      <h1>{counter}</h1>
+      <buttun>+</buttun>
   </div>
   );
 }
