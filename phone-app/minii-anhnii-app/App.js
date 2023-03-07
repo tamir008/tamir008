52e7292a1793cddbfdd8e022aa18c229d885b593
tamir008 <112378583+tamir008@users.noreply.github.com>
2023-03-07 12:51:17 +0800

    m plus
    1

--- phone-app/minii-anhnii-app/App.js
+++ phone-app/minii-anhnii-app/App.js
@@ -1,34 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Pressable, ScrollView, SafeAreaView, Button } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-    <ScrollView style={styles.scrollView}>
-<View>
-      <Pressable onPress={()=>{alert('switchImage')}} onLongPress={()=>{alert('onswitchImage')}}>
-      <Image style={{height:1050, width:150}} source={require('./assets/favicon.png')}></Image>
-      </Pressable> 
-      <Pressable onPress={()=>{alert('onpressText')}} onLongPress={()=>{alert('onlongpressText')}}>
-      <Text>my first app!</Text>
-      </Pressable>
-      <Button title='CLICKS' onPress={()=>alert('BUTTON')}/>
-      
-      <StatusBar style="auto" />
-      </View> 
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <View style={styles.view1}>
+
+      <Text style={styles.text1}>App</Text>
+    
+    </View>
+
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: StatusBar.currentHeight,
-  },
-  scrollView: {
-    backgroundColor: 'pink',
-    marginHorizontal: 20,
-    
-  },
-});
+view1: {flex: 1, alignItems: 'center', justifyContent:"center"
+},
+text1:{fontSize:30}
+})
